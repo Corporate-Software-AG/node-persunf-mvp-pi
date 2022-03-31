04d7b1b15416b2e43cce122265e610989d34c6f1
Thomas <65050898+tvonment@users.noreply.github.com>
2022-03-31 13:43:36 +0000

    node

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const port = process.env.PORT || 8080;
 const path = require('path');
 const short = require('short-uuid');
 
+var Protocol = require('azure-iot-device-amqp').Amqp;
+var Client = require('azure-iot-device').Client;
+var connectionString = process.env.DEVICE_CONNECTION_STRING;
+if (!connectionString) {
+    console.log('Please set the DEVICE_CONNECTION_STRING environment variable.');
+    process.exit(-1);
+}
+var client = Client.fromConnectionString(connectionString, Protocol);
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use('/favicon.ico', express.static('favicon.ico'));
@@ -11,9 +20,41 @@ app.use('/favicon.ico', express.static('favicon.ico'));
 app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-    res.render("home", { title: "POC QR", uuid: short.generate() });
+    res.render("home", { title: "POC Home" });
+})
+
+app.get('/qr', (req, res) => {
+    res.render("qr", { title: "POC QR", uuid: short.generate() });
+
+    client.open(function (err) {
+        if (err) {
+            console.error(err.toString());
+        } else {
+            console.log('client successfully connected to iot hub');
+            client.on('error', function (err) {
+                console.error(err.toString());
+            });
+
+            client.onDeviceMethod('onQrAcknowledged', function (request, response) {
+                console.log(request.payload);
+                var responsePayload = {
+                    message: 'message received'
+                };
+
+                response.send(200, responsePayload, function (err) {
+                    if (err) {
+                        console.error('Unable to send method response: ' + err.toString());
+                    } else {
+                        console.log('response to onQrAcknowledged sent.');
+                        res.render("home", { title: "POC Home" });
+                    }
+                });
+            });
+        }
+    });
 })
 
 app.listen(port, () => {
     console.log(`This app is listening at http://localhost:${port}`)
 })
+
