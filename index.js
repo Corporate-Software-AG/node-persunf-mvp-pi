9dea4059d98d11a94d47dc56f99fd4556deb907d
Thomas <65050898+tvonment@users.noreply.github.com>
2022-03-31 15:59:44 +0000

    test

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const port = process.env.PORT || 8080;
 const path = require('path');
 const short = require('short-uuid');
 
-var Protocol = require('azure-iot-device-amqp').Amqp;
-var Client = require('azure-iot-device').Client;
-var connectionString = process.env.DEVICE_CONNECTION_STRING;
-if (!connectionString) {
-    console.log('Please set the DEVICE_CONNECTION_STRING environment variable.');
-    process.exit(-1);
-}
-var client = Client.fromConnectionString(connectionString, Protocol);
-
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use('/favicon.ico', express.static('favicon.ico'));
@@ -25,33 +16,6 @@ app.get('/', (req, res) => {
 
 app.get('/qr', (req, res) => {
     res.render("qr", { title: "POC QR", uuid: short.generate() });
-
-    client.open(function (err) {
-        if (err) {
-            console.error(err.toString());
-        } else {
-            console.log('client successfully connected to iot hub');
-            client.on('error', function (err) {
-                console.error(err.toString());
-            });
-
-            client.onDeviceMethod('onQrAcknowledged', function (request, response) {
-                console.log(request.payload);
-                var responsePayload = {
-                    message: 'message received'
-                };
-
-                response.send(200, responsePayload, function (err) {
-                    if (err) {
-                        console.error('Unable to send method response: ' + err.toString());
-                    } else {
-                        console.log('response to onQrAcknowledged sent.');
-                        res.render("home", { title: "POC Home" });
-                    }
-                });
-            });
-        }
-    });
 })
 
 app.listen(port, () => {
