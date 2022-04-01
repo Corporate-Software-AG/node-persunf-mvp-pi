0b9a6a255e45ac865a4e6f04ab13bfd5d8f8a520
Thomas <65050898+tvonment@users.noreply.github.com>
2022-04-01 05:06:37 +0000

    add completion throw iot hub

--- index.js
+++ index.js
@@ -18,11 +18,53 @@ app.get('/qr', (req, res) => {
     res.render("qr", { title: "POC QR", uuid: short.generate() });
 })
 
+let state = { "complete": false }
+
 app.get('/status', (req, res) => {
-    res.json({"complete": false});
+    res.json(state);
 })
 
 app.listen(port, () => {
     console.log(`This app is listening at http://localhost:${port}`)
 })
+
+var Protocol = require('azure-iot-device-amqp').Amqp;
+var Client = require('azure-iot-device').Client;
+
+var connectionString = process.env.DEVICE_CONNECTION_STRING;
+if (!connectionString) {
+    console.log('Please set the DEVICE_CONNECTION_STRING environment variable.');
+    process.exit(-1);
+}
+var client = Client.fromConnectionString(connectionString, Protocol);
+
+client.open(function (err) {
+    if (err) {
+        console.error(err.toString());
+    } else {
+        console.log('client successfully connected to iot hub');
+        client.on('error', function (err) {
+            console.error(err.toString());
+        });
+
+        client.onDeviceMethod('onQrAcknowledged', function (request, response) {
+            console.log(request.payload);
+
+            state = request.payload;
+
+            var responsePayload = {
+                message: 'message received'
+            };
+
+            response.send(200, responsePayload, function (err) {
+                if (err) {
+                    console.error('Unable to send method response: ' + err.toString());
+                } else {
+                    console.log('response to onQrAcknowledged sent.');
+                }
+            });
+        });
+    }
+});
+
 
