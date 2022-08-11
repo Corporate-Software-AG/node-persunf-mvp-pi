e4efbe9bc7e9b010713ceaf5c65c0df13a6a662d
Thomas <65050898+tvonment@users.noreply.github.com>
2022-08-11 17:00:37 +0000

    add try catch in file upload block

--- index.js
+++ index.js
@@ -185,11 +185,15 @@ async function startFullScreenApp() {
 }
 
 function uploadLogs() {
-    let filePath = "/home/armasuisse/logs/servicestart.log";
-    let errorFilePath = "/home/armasuisse/logs/error.log";
-    let client = Client.fromConnectionString(connectionString, Protocol);
-    uploadLogFile(client, filePath, "-startup")
-    uploadLogFile(client, errorFilePath, "-error")
+    try {
+        let filePath = "/home/armasuisse/logs/servicestart.log";
+        let errorFilePath = "/home/armasuisse/logs/error.log";
+        let client = Client.fromConnectionString(connectionString, Protocol);
+        uploadLogFile(client, filePath, "-startup")
+        uploadLogFile(client, errorFilePath, "-error")
+    } catch (e) {
+        console.log("Log update failed: ", e)
+    }
 }
 
 function uploadLogFile(client, filePath, postfix) {
@@ -199,7 +203,7 @@ function uploadLogFile(client, filePath, postfix) {
             console.error('could not read file: ' + err.toString());
         } else {
             let fileStream = fs.createReadStream(filePath);
-            client.uploadToBlob(new Date().toISOString() + postfix + '.log', fileStream, fileStats.size, function (err, result) {
+            client.uploadToBlob(new Date().toISOString() + postfix + '.log', fileStream, fileStats.size, (err, result) => {
                 fileStream.destroy();
                 if (err) {
                     console.error('error uploading file: ' + err.constructor.name + ': ' + err.message);
