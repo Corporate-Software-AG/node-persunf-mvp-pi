fd10f2dd708d67ca19fbd50b30e3ee1a310fe6e0
Thomas <65050898+tvonment@users.noreply.github.com>
2022-08-11 12:22:47 +0000

    same as pi

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = process.env.PORT || 8080;
 const path = require('path');
 const Protocol = require('azure-iot-device-amqp').Amqp;
 const Client = require('azure-iot-device').Client;
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
@@ -12,9 +14,16 @@ app.use('/favicon.ico', express.static('favicon.ico'));
 app.use(express.urlencoded({ extended: true }));
 
 const connectionString = process.env.DEVICE_CONNECTION_STRING;
+const pin = process.env.PIN;
+
 
 if (!connectionString) {
     console.log('Configuration incomplete! Set the DEVICE_CONNECTION_STRING environment variables.');
+    process.exit(-1);
+}
+
+if (!pin) {
+    console.log('Configuration incomplete! Set the PIN environment variables.');
     process.exit(-1);
 }
 
@@ -28,7 +37,7 @@ let deviceConfig = {
 
 app.get('/', (req, res) => {
     state = { "complete": false }
-    res.render("qr", { title: "POC QR", device: deviceConfig });
+    res.render("qr", { title: "QR", device: deviceConfig });
 })
 
 app.get('/status', (req, res) => {
@@ -39,46 +48,121 @@ app.listen(port, async () => {
     console.log(`This app is listening at http://localhost:${port}`)
     let isConnected = !!await require('dns').promises.resolve('azure.com').catch(() => { });
     console.log("Connected to Internet: ", isConnected)
-
-    var exec = require('child_process').exec, child;
-    child = exec('sudo qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode', (error, stdout, stderr) => {
-        if (error !== null) {
-            console.log("Not available")
-            console.log(error)
-        } else {
-            console.log("Available")
-            console.log("Stdout", stdout)
-            console.log("Stderr", stderr)
-        }
-    });
-})
-
-let client = Client.fromConnectionString(connectionString, Protocol);
-client.open((error) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log('client successfully connected to iot hub');
-
-        client.getTwin((error, twin) => {
-            if (error) {
-                console.error('could not get twin')
-                process.exit(-1)
-            } else {
-                console.log('twin created');
-                twin.on('properties.desired', (delta) => {
-                    console.log('desired properties received:', JSON.stringify(delta));
-                    deviceConfig.location = delta.mzr;
-                    deviceConfig.verificationCode = delta.verificationCode
-                });
-            }
-        })
-
-        client.on('error', (error) => {
-            console.error(error);
-            process.exit(-1)
-        });
+    if (!isConnected) {
+        await setupCellular();
     }
+
+    startIotHubClient();
+    await startFullScreenApp();
+
 });
 
+async function setupCellular() {
+    try {
+        let commands = [
+            "sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online'",
+            'sudo ip link set wwan0 down',
+            'echo Y | sudo tee /sys/class/net/wwan0/qmi/raw_ip',
+            'sudo ip link set wwan0 up',
+            'sudo qmicli --device=/dev/cdc-wdm0 --uim-verify-pin=PIN1,' + pin,
+            'sudo qmicli --device=/dev/cdc-wdm0 --device-open-proxy --wds-start-network="ip-type=4,apn=gprs.swisscom.ch" --client-no-release-cid',
+        ]
+        await execCommands(commands);
+        await execDHCPCommand('sudo udhcpc -i wwan0')
+    } catch (e) {
+        console.error(e);
+        sleep(2000);
+        console.log("Retry Cellular Setup")
+        await setupCellular();
+    }
 
+}
+
+async function getStatus() {
+    const com = 'sudo qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode';
+    try {
+        let execOut = execCommand(com);
+        return execOut.split("\n\t")[1].split("'")[1];
+    } catch (e) {
+        console.log("Retry", e);
+        sleep(1000);
+        return execCommand(com);
+    }
+}
+
+
+async function execCommands(commands) {
+    for (let com of commands) {
+        try {
+            console.log("Exec: " + com);
+            await execCommand(com)
+        } catch (e) {
+            sleep(1000);
+            console.log(e)
+            console.log("Retry: " + com);
+            await execCommand(com)
+        }
+    }
+}
+
+async function execCommand(com) {
+    let { stdout, stderr } = await exec(com)
+    if (stderr) {
+        console.log("Error: ", stderr)
+        throw stderr;
+    } else {
+        console.log("Successful: ", stdout)
+        return stdout;
+    }
+}
+
+async function execDHCPCommand(com) {
+    let { stdout, stderr } = await exec(com)
+    if (stderr) {
+        console.log("Err. Log: ", stderr)
+    } else {
+        console.log("Successful: ", stdout)
+    }
+}
+
+function startIotHubClient() {
+    let client = Client.fromConnectionString(connectionString, Protocol);
+    client.open((error) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('client successfully connected to iot hub');
+
+            client.getTwin((error, twin) => {
+                if (error) {
+                    console.error('could not get twin')
+                } else {
+                    console.log('twin created');
+                    twin.on('properties.desired', (delta) => {
+                        console.log('desired properties received:', JSON.stringify(delta));
+                        deviceConfig.location = delta.mzr;
+                        deviceConfig.verificationCode = delta.verificationCode
+                    });
+                }
+            })
+
+            client.on('error', (error) => {
+                console.error(error);
+            });
+        }
+    });
+}
+
+async function startFullScreenApp() {
+    let commands = [
+        'export DISPLAY=unix:0.0',
+        'chromium-browser --kiosk http://localhost:8080'
+    ]
+    await execCommands(commands);
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
